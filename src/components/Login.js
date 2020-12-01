14bc58374d7f5cf7092cc3a032d9cc90c399328b
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-01 11:47:06 +0100

    added users ckick

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,40 @@
-import React, { Component} from 'react'
-import{connect} from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+import {fakeAuth} from '../utils/helpers'
 
 class Login extends Component {
 
     constructor(props) {
         super(props)
     }
-     handleLogin(user) {
-         console.log(user)
-     }
+    handleLogin(user) {
+        console.log(user)
+        fakeAuth.authentificate()
+        this.props.dispatch(setAuthedUser(user))
+    }
+   
     render() {
-        console.log(this.props.users)
-        return(
+        const usersItem = this.props.users.map((user) => (
+            <li key={user} onClick={() => { this.handleLogin(user) }}>
+                {user}
+            </li>
+        ))
+        return (
             <div>
                 Pick a user!
+                <ul>
+                    {usersItem}
+                </ul>
             </div>
         )
     }
 }
 function mapStateToProps({ users }) {
     return {
-      users: Object.keys(users)
+        users: Object.keys(users)
     }
-  }
-  
-  export default connect(mapStateToProps)(Login)
+}
+
+export default connect(mapStateToProps)(Login)
 
