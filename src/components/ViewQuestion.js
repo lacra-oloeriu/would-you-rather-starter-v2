4b6d05e19dc4ebb41f22a17bdef9ae7088527721
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-05 10:42:06 +0100

    connected ViewQuestion

--- src/components/ViewQuestion.js
+++ src/components/ViewQuestion.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class ViewQuestion extends Component {
   render() {
+    const { questions, users, authedUserId } = this.props;
+    console.log("authedUserId", authedUserId);
+    console.log("questions", questions);
+    console.log("users", users);
     return <div>ViewQuestion Component</div>;
   }
 }
 
-export default ViewQuestion;
+function mapStateToProps({ questions, users, authedUser }) {
+  return {
+    questions,
+    users,
+    authedUserId: authedUser,
+  };
+}
+
+export default connect(mapStateToProps)(ViewQuestion);
