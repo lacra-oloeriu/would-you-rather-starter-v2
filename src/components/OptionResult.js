8c220ecf822ad3352692c8ea2e80df68d6650660
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-11 13:58:33 +0100

    computed option values

--- src/components/OptionResult.js
+++ src/components/OptionResult.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Col, ProgressBar } from "react-bootstrap";
 
 class OptionResult extends Component {
   render() {
-    const { question, author,option } = this.props;
-    console.log('OptionResult data ',author, question, option)
+    const { question, author, option } = this.props;
+    console.log("OptionResult data ", question);
+
+    const countAll =
+      question.optionOne.votes.length + question.optionTwo.votes.length;
+
+    const thisVotes = question[option].votes.length;
+
+    const percent = (thisVotes * 100) / countAll;
+
+    console.log("option value", countAll, thisVotes, percent);
+    const progressInstance = (
+      <ProgressBar now={percent} label={`${percent} %`} />
+    );
     return (
       <Col sm={8}>
-        <div> Intrebare</div>
-        <div> Grafic</div>
-        <div> Text</div>
+        <div> {question[option].text}</div>
+        <div> {progressInstance}</div>
       </Col>
     );
   }
