45d38f4089a95c269e3848ac361ed3548f854eca
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-11 14:49:33 +0100

    filterd Questions

--- src/components/QuestionPreviewContainer.js
+++ src/components/QuestionPreviewContainer.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import QuestionPreview from "./QuestionPreview";
-import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav, Tab, Tabs } from "react-bootstrap";
 
 class QuestionPreviewContainer extends Component {
   render() {
-    const { questions, users } = this.props;
+    const { questions, users, questionIds, authedUser } = this.props;
 
     function getQuestion(id) {
       return questions[id];
@@ -17,22 +17,53 @@ class QuestionPreviewContainer extends Component {
       return users[authorId];
     }
 
+    let unanswered = [];
+    let answerd = [];
+
+    questionIds.forEach((id) => {
+      const theQuestion = questions[id];
+      const allVotes = theQuestion.optionOne.votes.concat(
+        theQuestion.optionTwo.votes
+      );
+
+      if (allVotes.includes(authedUser)) {
+        answerd.push(id);
+      } else {
+        unanswered.push(id);
+      }
+    });
+
     return (
       <div>
-        QuestionPreviewContainer
-        {this.props.questionIds.map((id) => (
-          <QuestionPreview question={getQuestion(id)} author={getAuthor(id)} />
-        ))}
+        <Tabs defaultActiveKey="Unanswered" id="uncontrolled-tab-example">
+          <Tab eventKey="Unanswered" title="Unanswered Question">
+            {unanswered.map((id) => (
+              <QuestionPreview
+                question={getQuestion(id)}
+                author={getAuthor(id)}
+              />
+            ))}
+          </Tab>
+          <Tab eventKey="answerd" title="Answerd Question">
+            {answerd.map((id) => (
+              <QuestionPreview
+                question={getQuestion(id)}
+                author={getAuthor(id)}
+              />
+            ))}
+          </Tab>
+        </Tabs>
       </div>
     );
   }
 }
 
-function mapStateToProps({ questions, users }) {
+function mapStateToProps({ questions, users, authedUser }) {
   return {
     questionIds: Object.keys(questions),
     questions,
     users,
+    authedUser,
   };
 }
 
