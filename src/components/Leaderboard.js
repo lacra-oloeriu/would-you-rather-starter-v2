ce6fa47ed817e8d33ae67fa3b2fc5b9fbc6a402e
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-13 13:15:34 +0100

    created leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,48 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Row, Col, Image, Card, Table,Badge } from "react-bootstrap";
+import LeaderItem from "./LeaderItem";
 
 class Leaderboard extends Component {
   render() {
     const { questions, users } = this.props;
     console.log(questions, users);
+
+    let rankItems = [];
+    rankItems.push("one", "two");
     return (
-      <Card>
-        <Card.Header>author name</Card.Header>
-        <Card.Body>
-          <Row>
-            <Col sm={4}>
-              <Image roundedCircle height="100px" />
-            </Col>
-
-            <Col sm={5}>
-              <Table hover size="sm">
-                <tbody>
-                  <tr>
-                    <td>Answerd Questions</td>
-                    <td>5000</td>
-                  </tr>
-
-                  <tr>
-                    <td>Created Questions</td>
-                    <td>6000</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </Col>
-
-            <Col sm={3}>
-              <Card.Title>Score</Card.Title>
-              <Card.Title>
-                  <Badge variant ='secondary'>
-                    800
-                  </Badge>
-
-              </Card.Title>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+      <div>
+        Leaderboard
+        {rankItems.map((item) => (
+          <LeaderItem />
+        ))}
+      </div>
     );
   }
 }
