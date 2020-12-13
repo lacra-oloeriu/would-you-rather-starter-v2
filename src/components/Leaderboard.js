428d0a48d9d023ca8684af4eb14af29f5941a0b7
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-13 12:48:52 +0100

    created leaderItam

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,14 +1,57 @@
-import React , {Component} from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Row, Col, Image, Card, Table,Badge } from "react-bootstrap";
 
 class Leaderboard extends Component {
-    render() {
-        return(
-            <div>
-                Leaderboard
-            </div>
-        )
-    }
+  render() {
+    const { questions, users } = this.props;
+    console.log(questions, users);
+    return (
+      <Card>
+        <Card.Header>author name</Card.Header>
+        <Card.Body>
+          <Row>
+            <Col sm={4}>
+              <Image roundedCircle height="100px" />
+            </Col>
 
+            <Col sm={5}>
+              <Table hover size="sm">
+                <tbody>
+                  <tr>
+                    <td>Answerd Questions</td>
+                    <td>5000</td>
+                  </tr>
+
+                  <tr>
+                    <td>Created Questions</td>
+                    <td>6000</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+
+            <Col sm={3}>
+              <Card.Title>Score</Card.Title>
+              <Card.Title>
+                  <Badge variant ='secondary'>
+                    800
+                  </Badge>
+
+              </Card.Title>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
-export default Leaderboard
+function mapSateToProps({ questions, users }) {
+  return {
+    questions,
+    users,
+  };
+}
+
+export default connect(mapSateToProps)(Leaderboard);
