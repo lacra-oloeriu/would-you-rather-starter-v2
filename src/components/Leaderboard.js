9a9145d63a32d63c1c400451ea52479d9f48e4bc
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-13 19:50:52 +0100

    computed points

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,15 +5,71 @@ import LeaderItem from "./LeaderItem";
 class Leaderboard extends Component {
   render() {
     const { questions, users } = this.props;
-    console.log(questions, users);
+
+    function getPointsFromQuestion(userKey, questionKey) {
+      let question = questions[questionKey];
+
+      let points = 0;
+      let answerd = 0;
+      let created = 0;
+      if (question.author === userKey) {
+        points++;
+        created++;
+      }
+
+      let allVotes = question.optionOne.votes.concat(question.optionTwo.votes);
+
+      if (allVotes.includes(userKey)) {
+        points++;
+        answerd++;
+      }
+
+      return {
+        points,
+        answerd,
+        created,
+        userKey,
+      };
+    }
+
+    let pointArray = [];
+    Object.keys(users).forEach((userKey) => {
+      let points = 0;
+      let answerd = 0;
+      let created = 0;
+
+      Object.keys(questions).forEach((questionKey) => {
+        let pointResult = getPointsFromQuestion(userKey, questionKey);
+        points = points + pointResult.points;
+        answerd = answerd + pointResult.answerd;
+        created = created + pointResult.created;
+      });
+      pointArray.push({ points, answerd, created, userKey });
+    });
+
+    function compare(a, b) {
+      if (a.points < b.points) {
+        return 1;
+      }
+      if (a.points > b.points) {
+        return -1;
+      }
+      return 0;
+    }
+
+    pointArray.sort(compare);
 
     let rankItems = [];
     rankItems.push("one", "two");
     return (
       <div>
         Leaderboard
-        {rankItems.map((item) => (
-          <LeaderItem />
+        {pointArray.map((item) => (
+          <LeaderItem
+            key={item.userKey}
+            user={users[item.userKey]}
+            pointObject={item}
+          />
         ))}
       </div>
     );
