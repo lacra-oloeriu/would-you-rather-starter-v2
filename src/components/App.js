3603f4133dda70095c60b0bb9e20cb4e72cd0d3b
Lacramioara <lacramioara.oloeriu@gmail.com>
2020-12-15 11:35:14 +0100

    fixt router bug

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
         <Redirect
           to={{
             pathname: "/login",
-            state: { from: props.location },
+            state: props.location ,
           }}
         />
       )
